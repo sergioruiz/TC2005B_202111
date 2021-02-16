7cc0c9c7912e70e846f1541516cc1945ede1c638
sergioruiz <4043216+sergioruiz@users.noreply.github.com>
2021-02-16 13:33:33 -0600

    Trabajo con Prefabs
    Prefabs
    UI

---
+++ Assets/ControlarUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlarUI : MonoBehaviour
+{
+    public TMPro.TMP_Text helloSteamLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleHelloButton()
+    {
+        if(helloSteamLabel.enabled == true)
+        {
+            helloSteamLabel.enabled = false;
+            Debug.Log("Deshabilitar etiqueta");
+        }
+        else
+        {
+            helloSteamLabel.enabled = true;
+            Debug.Log("Habilitar etiqueta");
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Scene02");
+    }
+}
---
+++ Assets/CubeMaker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMaker : MonoBehaviour
+{
+    public GameObject specialCubePrefab;
+    List<GameObject> myPrefabs; // Declare
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPrefabs = new List<GameObject>(); // Instantiate
+        Vector3 location = Vector3.zero;
+        Quaternion rotation = Quaternion.identity;
+        for (int i = 0; i < 10; i++)
+        {
+            location.x = i * 10;
+            myPrefabs.Add(Instantiate(specialCubePrefab, location, rotation));
+        }
+        Rigidbody rb = myPrefabs[4].GetComponent<Rigidbody>();
+        rb.AddForce(rb.transform.forward * 5000.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            // Obtener Game Object 'i'
+            GameObject go = myPrefabs[i];
+            go.transform.Rotate(new Vector3(0, 0.1f, 0));
+        }
+    }
+
+
+}
